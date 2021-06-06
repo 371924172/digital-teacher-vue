1929417bd82b25e7d8911332474db760df59aba6
Wangjian530 <9186182+wangjian530@user.noreply.gitee.com>
2021-06-06 19:19:43 +0800

    组织，学院，班级，专业数据表

---
+++ frontend/src/views/class/list.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-table :data="classList" border style="width: 100%">
+    <el-table-column prop="college" label="所在学院" width="125" align="center">
+      <template slot-scope="scope">
+        <el-input v-model="scope.row.status"></el-input>
+      </template>
+    </el-table-column>
+    <el-table-column prop="major" label="所属专业" width="150" align="center">
+      <template slot-scope="scope">
+        <el-input v-model="scope.row.status"></el-input>
+      </template>
+    </el-table-column>
+    <el-table-column prop="className" label="班级名称" width="130" align="center">
+      <template slot-scope="scope">
+        <el-input v-model="scope.row.status"></el-input>
+      </template>
+    </el-table-column>
+    <el-table-column prop="classIntroduction" label="班级说明" width="500" align="center">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.status"></el-input>
+        </template>
+    </el-table-column>
+    <el-table-column prop="foundationTime" label="成立时间" width="130" align="center">
+      <template slot-scope="scope">
+        <el-input v-model="scope.row.status"></el-input>
+      </template>
+    </el-table-column>
+    <el-table-column prop="studyYearNumber" label="学习年数" width="120" align="center">
+    <template slot-scope="scope">
+      <el-input v-model="scope.row.status"></el-input>
+    </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+    export default {
+        name: "list.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
