411f9c5f662a8d5af41367cf0cc842a4b2b79ba3
zxy <371924172@qq.com>
2021-09-09 11:14:54 +0800

    test

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,5 +1,61 @@
 <template>
   <div class="dashboard-container">
+    <el-row>
+      <el-col :span="4">
+        <el-select
+          v-model="organizationId"
+          placeholder="请选择学校或者组织"
+          @change="getCollegeList(organizationId)"
+        >
+          <el-option
+            v-for="item in organizationList"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          >
+          </el-option> </el-select
+      ></el-col>
+      <el-col :span="4">
+        <el-select
+          v-model="collegeId"
+          placeholder="请选择学院"
+          @change="getMajorList(collegeId)"
+        >
+          <el-option
+            v-for="item in collegeList"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          >
+          </el-option> </el-select
+      ></el-col>
+      <el-col :span="4">
+        <el-select
+          v-model="majorId"
+          placeholder="请选择专业"
+          @change="getClassList(majorId)"
+        >
+          <el-option
+            v-for="item in majorList"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          >
+          </el-option> </el-select></el-col
+      ><el-col :span="4">
+        <el-select
+          v-model="classId"
+          placeholder="请选择班级"
+          @change="getData(classId)"
+        >
+          <el-option
+            v-for="item in classList"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          >
+          </el-option> </el-select></el-col
+    ></el-row>
     <el-row
       ><el-col :span="12">
         <div id="problemChart" style="width: 100%; height: 500px"></div>
@@ -14,6 +70,10 @@
 <script>
 import { mapGetters } from "vuex";
 import { recordByClass, recordByProblem } from "@/api/dashboard";
+import { getOrganizationList } from "@/api/organization";
+import { getCollegeListByOrganization } from "@/api/college";
+import { getMajorByCollege } from "@/api/major";
+import { getClassByMajor } from "@/api/class";
 import * as echarts from "echarts";
 export default {
   username: "Dashboard",
@@ -22,6 +82,14 @@ export default {
   },
   data() {
     return {
+      organizationList: [],
+      collegeList: [],
+      majorList: [],
+      classList: [],
+      organizationId: "",
+      collegeId: "",
+      majorId: "",
+      classId: "",
       record: [
         {
           problem_id: 1,
@@ -110,14 +178,44 @@ export default {
     };
   },
   methods: {
-    getData() {
-      recordByClass().then((response) => {
+    getOrganizationList() {
+      getOrganizationList().then((response) => {
+        this.organizationList = response.data;
+      });
+    },
+    getCollegeList(id) {
+      var data = { id: id };
+      getCollegeListByOrganization(data).then((response) => {
+        this.collegeList = response.data;
+      });
+      getStudentByOrganization(data).then((response) => {
+        this.tableData = response.data;
+      });
+    },
+    getMajorList(id) {
+      var data = { id: id };
+      getMajorByCollege(data).then((response) => {
+        this.majorList = response.data;
+      });
+      getStudentByCollege(data).then((response) => {
+        this.tableData = response.data;
+      });
+    },
+    getClassList(id) {
+      var data = { id: id };
+      getClassByMajor(data).then((response) => {
+        this.classList = response.data;
+      });
+    },
+    getData(classId) {
+      var data = { classId: classId };
+      recordByClass(data).then((response) => {
         this.classOption.xAxis.data = response.data.user_name;
         this.classOption.series[0].data = response.data.percent;
         var classChart = echarts.init(document.getElementById("classChart"));
         this.classOption && classChart.setOption(this.classOption);
       });
-      recordByProblem().then((response) => {
+      recordByProblem(data).then((response) => {
         this.problemOption.yAxis.data = response.data.problem_name;
         this.problemOption.series[0].data = response.data.percent;
         var problemChart = echarts.init(
@@ -132,8 +230,7 @@ export default {
     // },
   },
   mounted() {
-    this.getData();
-    console.log(this.$store.getters.roles);
+    this.getOrganizationList();
   },
 };
 </script>
